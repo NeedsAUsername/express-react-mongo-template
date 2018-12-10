1352be8aa6f7364eea263bad7b8582a107cd1bad
Kevin Chi <kevinchi118@gmail.com>
2018-12-10 18:23:49 -0500

    node_modules changes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const server = express();
+const path = require('path');
+// To handle HTTP POST request in Express.js version 4 and above,
+// body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
+const bodyParser = require('body-parser');
+const homeRoute = require('./routes/home');
+
+server.use(express.static(__dirname + '/public'));
+server.use(homeRoute);
+
+// Moved to the routes folder
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'view', 'index.html'));
+// })
+
+const port
+server.listen(3000);
