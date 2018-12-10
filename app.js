8be09595bc07ed1eb01f891c9fe5c79ecb6056a3
Kevin Chi <kevinchi118@gmail.com>
2018-12-10 12:46:18 -0500

    adds basic get route, listen, and public folders

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const path = require('path');
 // To handle HTTP POST request in Express.js version 4 and above,
 // body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
 const bodyParser = require('body-parser');
+
+// Move to a routes folder later
+app.get('/', (req, res) => {
+  res.send('hello');
+})
+app.listen(3000);
