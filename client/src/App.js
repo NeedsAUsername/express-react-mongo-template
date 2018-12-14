2ce5c66cb0f7abd7e05465e1feedd7bbafbcec51
Kevin Chi <kevinchi118@gmail.com>
2018-12-13 21:49:38 -0500

    adds routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Users from './users';
 import Auth from './auth';
@@ -6,10 +7,13 @@ import Auth from './auth';
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Users />
-        <Auth />
-      </div>
+      // Later: Setup routes for different pages(user profile, users search, posts, etc.)
+      <Router>
+        <div className="app">
+          <Users />
+          <Auth />
+        </div>
+      </Router>
     );
   }
 }
